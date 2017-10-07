4ff5f46b4eddf6613eeb4d7661aadafbbdb9d76c
Joseph NGOMA <jngoma@e5r6p2.wethinkcode.co.za>
2017-10-07 11:10:41 +0200

    Update

---
+++ day_02/ex01/one_more_time.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+//$timeFirst  = strtotime('1969-12-31 00:00:00');
+//$timeSecond = strtotime('2013-11-12 12:02:21');
+//$differenceInSeconds = $timeSecond - $timeFirst;
+mktime()
+echo $differenceInSeconds. "\n";
+?>
